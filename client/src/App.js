89187a1f8bfedbe68ae09e333a50cd37ffebed49
Mihirss <mihirsoniTM@gmail.com>
2021-11-10 16:30:36 +0530

    Alert set in REDUX STORE

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
 import "./App.css";
 import React, { Fragment } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Layouts/Navbar";
 import Landing from "./Components/Layouts/Landing";
+import Login from "./Components/auth/Login";
+import Register from "./Components/auth/Register";
+
+//Redux is added here...
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-      </Routes>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
